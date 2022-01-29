03216c8e99f421f05e65447ccdcdabae38db38a7
alexyyyyytytytytyt <95461634+alexyyyyytytytytyt@users.noreply.github.com>
2022-01-29 10:09:12 -0800

    loop.py file added

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,21 +1,52 @@
-from itertools import count
 import os
 import csv
 
-csvpath = os.path.join("Resources1", "election_data.csv")
+csvpath = os.path.join("Documents", "python-challenge", "PyPoll", "Resources1", "election_data.csv")
 
 count = 0
 
 with open(csvpath) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
     counter_Khan = 0
+    c_correy = 0
+    li = 0
+    tooley = 0
 
     for row in csv_reader:
         # print(row[0], row[1], row[2])
         if row[2] == str("Khan"):
             counter_Khan = counter_Khan + 1
-
-
+        if row[2] == str("Correy"):
+            c_correy = c_correy + 1
+        if row[2] == str("Li"):
+            li = li + 1
+        if row[2] == str("O'Tooley"):
+            tooley = tooley + 1
+            
+        max_winner = max(counter_Khan, c_correy, li, tooley)
         count = count + 1
-    print(count)
-    print(counter_Khan)
+
+    khan_percent = round(int((counter_Khan)/(count - 1)*100), 0)
+    tooley_percent = round(((tooley/(count - 1))*100), 0)
+    li_percent = round(((li/(count - 1))*100), 0)
+    correy_percent = round(c_correy/(count - 1)*100, 0)
+
+
+    print("Election Results")
+    print("-------------------------")  
+    print("Total Votes: " + str(count))
+    print("-------------------------")
+    print("Khan: " + str(khan_percent) + "% " + str(counter_Khan))
+    print("Correy: " + str(correy_percent) + "% " + str(c_correy))
+    print("Li: " + str(li_percent) + "% " + str(li))
+    print("O'Tooley: " + str(tooley_percent) + "% " + str(tooley))
+    print("-------------------------")
+
+if counter_Khan > c_correy and counter_Khan > li and counter_Khan > tooley:
+    print("Winner: Khan")
+if c_correy > counter_Khan and c_correy > li and c_correy > tooley:
+    print("Winner: Correy")
+if li > c_correy and li > counter_Khan and li > tooley:
+    print("Winner: Li")
+if tooley > c_correy and tooley > li and tooley > counter_Khan:
+    print("Winner: O'Tooley")
