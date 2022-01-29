2fe543f4308a3c9e6e5b9ae21e3b228cfcb7c67d
alexyyyyytytytytyt <95461634+alexyyyyytytytytyt@users.noreply.github.com>
2022-01-29 10:07:47 -0800

    loop.py file added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,55 @@
+from operator import index
 import os
 import csv
+from pdb import line_prefix
 
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+csvpath = os.path.join("Documents", "python-challenge", "PyBank", "Resources", "budget_data.csv")
+
+
+profit_list = []
+months = []
+sum_months = 0
+monthly_change = []
+total = 0
+with open(csvpath) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+    header = next(csv_reader, None)
+    for row in csv_reader:
+        profit_list.append(int(row[1]))
+        months.append(row[0])
+        sum_months = sum_months + 1
+        total = total + int(row[1])
+
+    for i in range(1, len(profit_list)):
+        monthly_change.append(profit_list[i] - profit_list[i - 1])
+
+    max_change = 0
+    min_change = 0
+    max_month = ""
+    for months, change in zip(months[1:], monthly_change):
+        if change > max_change:
+            max_change = change
+            max_month = months
+        if change < min_change:
+            min_change = change
+            min_month = months
+
+    print("Financial Analysis")
+    print("---------------------------------")
+
+    print("Total Months:", str(sum_months))
+    print("Total: " + "$" + str(total))
+
+    print("Greatest Increase in Profits:", str(max_month), '$' + str(max_change))
+    print("Greatest Decrease in Profits:", str(min_month), "$" +  str(min_change))
+
+
+
+
+
+
+
+
 
 
 
